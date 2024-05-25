860b60060b4d98d04ea5d010c5bf62e49546db68
imadBelattar <imad.belattar.dev@gmail.com>
2024-05-25 02:29:40 +0100

    display and list all intervention for administrateur from backend

--- gestionHoraire-Angular/src/app/components/card/card.component.ts
+++ gestionHoraire-Angular/src/app/components/card/card.component.ts
@@ -11,24 +11,10 @@ import { MatIcon, MatIconModule } from '@angular/material/icon';
   styleUrl: './card.component.css'
 })
 export class CardComponent {
-  constructor() { 
-    this.title = 'Title: Card Title';
-    this.subtitles = [
-      'Subtitle 1: subtitle content',
-      'Subtitle 2: subtitle content',
-      'Subtitle 3: subtitle content'
-    ];
-    this.pElementsContent = [
-      'This is a card component',
-      'It is used to display information in a card format',
-      'It has a title, subtitles and a list of paragraphs'
-    
-    ];
-    this.wantButtons = true;
-  }
-  @Input() title: string;
-  @Input() subtitles: string[]; 
-  @Input() pElementsContent: string[];
-  @Input() wantButtons: boolean;
+  constructor() {}
+  @Input() title!: string;
+  @Input() subtitles!: string[]; 
+  @Input() pElementsContent!: string[];
+  @Input() wantButtons: boolean = true;
 
 }
